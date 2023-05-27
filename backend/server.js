d086eb3727f6b3a9d22e9671221d3602d306e9db
Vignesh <96247274+vignesh-wikki@users.noreply.github.com>
2023-05-27 12:40:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://hall-booking-system.vercel.app/",
   })
 );
 
